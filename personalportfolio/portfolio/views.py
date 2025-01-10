9e30fc0e414db42858fa1f6e25bddc725ff39828
joana2727 <jg320608@gmail.com>
2025-01-10 08:33:33 +0800

    first commit

---
+++ personalportfolio/portfolio/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'portfolio/home.html')
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def skills(request):
+    return render(request, 'portfolio/skills.html')
+
+def projects(request):
+    return render(request, 'portfolio/projects.html')
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
